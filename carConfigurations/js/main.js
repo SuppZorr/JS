7109beaec9a87bcdc0b96ca78f9dd59a1a34927e
SuppZorr <supp.zorr@gmail.com>
2020-09-25 14:52:40 +0300

    initial

--- carConfigurations/js/main.js
+++ carConfigurations/js/main.js
@@ -5,35 +5,7 @@
 // ["1", "2", "3", "4", "5", "6", "7", "8", "9"]  сдвиг на i+2
 // ["1", "4", "7", "2", "5", "8", "3", "6", "9"]
 
-
-
-
-// const allConfigurations = {                   
-//     color : {
-//         Y : "yellow",  0
-//         G : "green",   1
-//         R : "red",    2
-//         B : "blue",    3
-//         O : "orange",   4
-//     },
-//     transmission : {
-//         A : "automatic",   0
-//         M : "manual",   1
-//     },
-//     conditioner : {
-//         I : "installed",  0
-//         U : "uninstalled",   1
-//     },
-//     interior : {
-//         L : "leather",   0
-//         T : "textile",   1
-//         C : "combined",    2
-
-//     }
-// };
-
  
-
 // 0001GMUT
 // 0002YAIC
 
@@ -41,147 +13,45 @@
 // XXXX     0112
 
 
-function randomInteger(min, max) {
-  // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-
-let arr = [];
-let item = null;
-
-for (i=0; i<500; i++) {
-     
-     let x = randomInteger(0,4);
-     let y = randomInteger(0,1);
-     let z = randomInteger(0,1);
-     let w = randomInteger(0,2);
-
-    if (x==0) {
-        x='Y'
-    }
-    else if (x==1){
-        x='G'
-    }
-    else if (x==2){
-        x='R'
-    }
-    else if (x==3){ 
-        x='B'
-    }
-    else x='O';
-
-    if (y==0) {
-        y='A'
-    }
-    else y='M';
-
-    if (z==0){
-        z='I'
-    }
-    else z='U';
-
-    if (w==0) {
-        w='L'
-    }
-    else if (w==1){
-        w='T'
-    }
-    else w='C';
-
-            
-                item = `${x}${y}${z}${w}`;
-                 
-                arr.indexOf(item) == -1 ?   arr.push(item) :   arr;
-                 
-       
-                
-};
-
-arr.sort();
-// console.log(arr);
-
- 
-
-let models = [];
-const configList = {};
-
-// тут мы делаем комбинации 0000 - 9999 
-for (i = 0 ; i< arr.length ; i++) {                              
-    if  (String(i).length < 2) {
-        models[i] = `config_000${i}${arr[i]}`;
-       
-    }
-    else  {
-        models[i] = `config_00${i}${arr[i]}`;
-    }
-    
-};
-
- 
-// console.log(models); 
  
 
  
-for (i = 0 ; i< models.length ; i++) { 
-    
-    configList[models[i]] = {
-        color : "",
-        transmission : "",
-        conditioner : "",
-        interior : "",
-        
-    };
+let i = 0;
 
-     
+let mainArr = [];
+let colorArr = ['yellow','green','red','blue','orange'];
+let transmissionArr = ['automatic','manual'];
+let conditionerArr = ['installed','uninstalled'];
+let interiorArr = ['leather','textile','combined'];
 
-    let elem = models[i];
-
-   if (elem[11] == 'B') {
-    configList[models[i]].color = 'blue'
-   }
-   else if (elem[11] == 'G') {
-    configList[models[i]].color = 'green'
-   }
-   else if (elem[11] == 'R') {
-    configList[models[i]].color = 'red'
-   }
-   else if (elem[11] == 'O') {
-    configList[models[i]].color = 'orange'
-   }
-   else   {
-    configList[models[i]].color = 'yellow'
-   };
-
-   if (elem[12] == "A") {
-   configList[models[i]].transmission = 'automatic'
-   }
-   else {
-    configList[models[i]].transmission = 'manual'
-    };
-
-    if (elem[13] == "I") {
-    configList[models[i]].conditioner = 'installed'
-    }
-    else {
-    configList[models[i]].conditioner = 'uninstalled'
-    };
-
-    if (elem[14] == "L") {
-    configList[models[i]].interior = 'leather'
-    }
-    else if (elem[14] == "T") {
-    configList[models[i]].interior = 'textile'
-    }
-    else  {
-        configList[models[i]].interior = 'combined'
+for (let colorIndex = 0; colorIndex < colorArr.length; colorIndex++) {
+    for (let transmissionIndex = 0; transmissionIndex < transmissionArr.length; transmissionIndex++) {
+      for (let conditionerIndex = 0; conditionerIndex < conditionerArr.length; conditionerIndex++) {
+        for (let interiorIndex = 0; interiorIndex < interiorArr.length; interiorIndex++) {
+          mainArr.push({
+            code: (`${i}${(colorArr[colorIndex][0]).toUpperCase()}${(transmissionArr[transmissionIndex][0]).toUpperCase()}${(conditionerArr[conditionerIndex][0]).toUpperCase()}${(interiorArr[interiorIndex][0]).toUpperCase()}`).padStart(8, "0"),
+            color:  colorArr[colorIndex],
+            transmission:  transmissionArr[transmissionIndex],
+            conditioner: conditionerArr[conditionerIndex],
+            interior: interiorArr[interiorIndex],
+          });
+          i++;
         }
+      }
+    }
+  }
 
-};
+
+console.log(mainArr)
+
+
+
+
+
+
 
  
-console.log(configList);
-
+ 
  
  
 
